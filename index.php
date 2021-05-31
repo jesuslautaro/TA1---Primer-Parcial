02d490247b88b91e013c2ac1a4cfe1915381930e
Jesus Lautaro <jesus.lautaro99@gmail.com>
2021-05-31 20:07:59 -0300

    Commit 1-Formulario

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ParcialPHP</title>
+</head>
+<body>
+    <form id="datos" action="./formulario.php" method="post">
+    Nombre: <input type="text" name="Nombre" id="Nombre"> <br>
+    Apellido: <input type="text" name="Apellido" id="Apellido"> <br>
+    Telefono: <input type="integer" name="Telefono" id="Telefono"> <br>
+    Edad: <input type="integer" name="Edad" id="Edad"> <br>
+    Fecha De Nacimiento: <input type="text" name="FechaDeNac" id="FechaDeNac"> <br>
+    Email: <input type="text" name="nombre" id="Email"> <br>
+    <button type="submit">Mostrar </button><br>
+</form>
+</html>
+
