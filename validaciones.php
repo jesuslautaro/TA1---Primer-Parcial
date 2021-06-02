b459f3119f3e4acc8863d8362e3ea0fc770251dc
Jesus Lautaro <jesus.lautaro99@gmail.com>
2021-06-01 22:56:13 -0300

    Commit 5- Intento de validacion-2

--- validaciones.php
+++ validaciones.php
@@ -1,11 +1,21 @@
 <?php 
 
- function validacionDeCampos($datos){
+ function agregado(){
+    echo "Se agrega persona" ;
+
+}
+
+function validacionDeCampos($datos){
         if($datos['Nombre'] === "" || $datos['Apellido'] === "" || $datos['Telefono'] === "" || $datos['FechaDeNac'] === "" || $datos['Edad'] === "" || $datos['Email'] === ""){
             error_log("No completó los campos. Complete todos los campos");
             return false;
         }
-        return true;
+        return agregado();
     }
 
+    if($_SERVER['REQUEST_METHOD'] === 'GET'){
+        header("HTTP/1.1 500 Forbidden");
+        die("Forbidden");
+    }
   
+    
